617b6a02d06c784ea7a4d5937fb515461ed2c2a7
ZTX <thoover@iastate.edu>
2024-06-27 13:13:02 -0500

    Update multi_ue.py to support No GUI

--- multi_ue.py
+++ multi_ue.py
@@ -8,63 +8,20 @@
 # Title: 2UE Flowgraph
 # GNU Radio version: 3.8.1.0
 
-from distutils.version import StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
-
 from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import zeromq
-from gnuradio.qtgui import Range, RangeWidget
-from gnuradio import qtgui
 
-class multi_ue(gr.top_block, Qt.QWidget):
+class multi_ue(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "2UE Flowgraph")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("2UE Flowgraph")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "multi_ue")
-
-        try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
 
         ##################################################
         # Variables
@@ -72,22 +29,10 @@ class multi_ue(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate = 23.04e6
         self.min_gain = min_gain = 0
         self.max_gain = max_gain = 1
-        self.cell_gain2 = cell_gain2 = 0.050
-        self.cell_gain1 = cell_gain1 = 0.080
-        self.cell_gain0 = cell_gain0 = 0.050
 
         ##################################################
         # Blocks
         ##################################################
-        self._cell_gain2_range = Range(min_gain, max_gain, 0.0001, 0.050, 200)
-        self._cell_gain2_win = RangeWidget(self._cell_gain2_range, self.set_cell_gain2, 'cell_gain2', "counter_slider", float)
-        self.top_grid_layout.addWidget(self._cell_gain2_win)
-        self._cell_gain1_range = Range(min_gain, max_gain, 0.0001, 0.080, 200)
-        self._cell_gain1_win = RangeWidget(self._cell_gain1_range, self.set_cell_gain1, 'cell_gain1', "counter_slider", float)
-        self.top_grid_layout.addWidget(self._cell_gain1_win)
-        self._cell_gain0_range = Range(min_gain, max_gain, 0.0001, 0.050, 200)
-        self._cell_gain0_win = RangeWidget(self._cell_gain0_range, self.set_cell_gain0, 'cell_gain0', "counter_slider", float)
-        self.top_grid_layout.addWidget(self._cell_gain0_win)
         self.zeromq_req_source_1 = zeromq.req_source(gr.sizeof_gr_complex, 1, 'tcp://localhost:2000', 100, False, -1)
         self.zeromq_req_source_0_0_0 = zeromq.req_source(gr.sizeof_gr_complex, 1, 'tcp://localhost:2008', 100, False, -1)
         self.zeromq_req_source_0_0 = zeromq.req_source(gr.sizeof_gr_complex, 1, 'tcp://localhost:2007', 100, False, -1)
@@ -98,12 +43,12 @@ class multi_ue(gr.top_block, Qt.QWidget):
         self.zeromq_rep_sink_0 = zeromq.rep_sink(gr.sizeof_gr_complex, 1, 'tcp://*:2009', 100, False, -1)
         self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_multiply_const_vxx_0_1_0_0 = blocks.multiply_const_cc(cell_gain2)
-        self.blocks_multiply_const_vxx_0_1_0 = blocks.multiply_const_cc(cell_gain1)
-        self.blocks_multiply_const_vxx_0_0_1 = blocks.multiply_const_cc(cell_gain2)
-        self.blocks_multiply_const_vxx_0_0_0 = blocks.multiply_const_cc(cell_gain0)
-        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_cc(cell_gain1)
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(cell_gain0)
+        self.blocks_multiply_const_vxx_0_1_0_0 = blocks.multiply_const_cc(0.05)
+        self.blocks_multiply_const_vxx_0_1_0 = blocks.multiply_const_cc(0.08)
+        self.blocks_multiply_const_vxx_0_0_1 = blocks.multiply_const_cc(0.05)
+        self.blocks_multiply_const_vxx_0_0_0 = blocks.multiply_const_cc(0.05)
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_cc(0.08)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(0.05)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
 
 
@@ -127,11 +72,6 @@ class multi_ue(gr.top_block, Qt.QWidget):
         self.connect((self.zeromq_req_source_0_0_0, 0), (self.blocks_multiply_const_vxx_0_0_1, 0))
         self.connect((self.zeromq_req_source_1, 0), (self.blocks_throttle_0, 0))
 
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "multi_ue")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
-
     def get_samp_rate(self):
         return self.samp_rate
 
@@ -152,58 +92,26 @@ class multi_ue(gr.top_block, Qt.QWidget):
     def set_max_gain(self, max_gain):
         self.max_gain = max_gain
 
-    def get_cell_gain2(self):
-        return self.cell_gain2
-
-    def set_cell_gain2(self, cell_gain2):
-        self.cell_gain2 = cell_gain2
-        self.blocks_multiply_const_vxx_0_0_1.set_k(self.cell_gain2)
-        self.blocks_multiply_const_vxx_0_1_0_0.set_k(self.cell_gain2)
-
-    def get_cell_gain1(self):
-        return self.cell_gain1
-
-    def set_cell_gain1(self, cell_gain1):
-        self.cell_gain1 = cell_gain1
-        self.blocks_multiply_const_vxx_0_0.set_k(self.cell_gain1)
-        self.blocks_multiply_const_vxx_0_1_0.set_k(self.cell_gain1)
-
-    def get_cell_gain0(self):
-        return self.cell_gain0
-
-    def set_cell_gain0(self, cell_gain0):
-        self.cell_gain0 = cell_gain0
-        self.blocks_multiply_const_vxx_0.set_k(self.cell_gain0)
-        self.blocks_multiply_const_vxx_0_0_0.set_k(self.cell_gain0)
-
 
 
 def main(top_block_cls=multi_ue, options=None):
-
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
-    tb.start()
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
-        Qt.QApplication.quit()
+        tb.stop()
+        tb.wait()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.aboutToQuit.connect(quitting)
-    qapp.exec_()
+    tb.start()
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
 
 
 if __name__ == '__main__':
